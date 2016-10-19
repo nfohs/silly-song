08db9b0999fc417157f915fa4359d50186bdfba9
nfohs <nick.fohs@gmail.com>
2016-10-18 17:33:56 -0700

    working app, implemented the 'polish' options to change to 'Done', capitalize the string, and re-display text

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(_ sender: AnyObject) {
+        lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+    }
 
+}
+
+func shortNamefromName(name: String) -> String {
+    return name.substring(from: name.index(after: name.startIndex))
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNamefromName(name: fullName)
+    
+    let songWithName = lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName.capitalized)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return songWithName
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
 }
 
